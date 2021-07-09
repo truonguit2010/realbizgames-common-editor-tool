a83497754049eb0c83ca191ffc4e5ce3ad588c27
DarkNight <truonguit2010@gmail.com>
2021-07-09 08:22:25 +0700

    - Init the project.

---
+++ Editor/Tool/MonoHandlerInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(MAMonoBehaviour), true)]
+public class MonoHandlerInspector : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		MAMonoBehaviour mono = (MAMonoBehaviour)target;
+		mono.GUIEditor();
+
+	}
+
+
+}
